63887e5880baa76f2e3858de9c5a8a92bbac667a
alexandrezenas <94380403+AlexandreZenas@users.noreply.github.com>
2023-10-04 14:19:28 -0300

    ''

--- src/App.js
+++ src/App.js
@@ -6,27 +6,7 @@
   const containerContentSpansArray = Array.from(containerContentSpans);
   const sliderNavegationLiArray = Array.from(sliderNavegationLi);
 
-  function scrollDown(i) {
-          sliderNavegationLiArray[i+1].classList.add('activeSlide')                 
-          sliderPagesArray[i+1].classList.add('sliderShow');
-          sliderPagesArray[i+1].classList.remove('sliderUp');
-          containerContentSpansArray[i+1].classList.add('animationSpan');         
-          containerContentSpansArray[i+1].classList.remove('spanPageUp');  
-          //adiciona e remove as classes atuais
-          sliderNavegationLiArray[i].classList.remove('activeSlide')
-          sliderPagesArray[i].classList.remove('sliderShow');
-          sliderPagesArray[i].classList.add('sliderUp');
-          containerContentSpansArray[i].classList.remove('animationSpan');  
-          containerContentSpansArray[i].classList.add('spanPageUp');  
-          //remove as classes anteriores
-          if(i > 0){
-            sliderPagesArray[i-1].classList.remove('sliderUp');  
-            containerContentSpansArray[i-1].classList.remove('spanPageUp');
-          }           
-  }
-  function scrollUp(i) {
-               
-  }
+  const arrayTest = [sliderPagesArray, containerContentSpansArray, sliderNavegationLiArray]
 
   for(let i = 0; i < sliderPagesArray.length; i++){
 
@@ -35,53 +15,52 @@
         
         if( y > 0 && i < 4){     
           //adiciona e remove as classes posteriores  
-          scrollDown(i)
+          arrayTest.forEach((item) => {
+            item[i+1].classList.add('showElement');
+            item[i+1].classList.remove('mouseScrolled');
+            item[i].classList.add('mouseScrolled');
+            item[i].classList.remove('showElement');
+            if(i  > 0){
+              item[i-1].classList.remove('mouseScrolled')
+            } 
+          })     
         }
         else if (y < 0 && i <= 4){
           //adiciona e remove as classes anteriores
-          if( i > 0){
-            sliderNavegationLiArray[i-1].classList.add('activeSlide');
-            sliderPagesArray[i-1].classList.add('sliderShow');
-            sliderPagesArray[i-1].classList.remove('sliderUp');
-            containerContentSpansArray[i-1].classList.add('animationSpan');
-          }
-          containerContentSpansArray[i-1].classList.remove('spanPageUp');              
-          //adiciona e remove as classes atuais
-          sliderPagesArray[i].classList.remove('sliderShow');
-          sliderPagesArray[i].classList.add('sliderUp');
-          sliderNavegationLiArray[i].classList.remove('activeSlide');
-          containerContentSpansArray[i].classList.remove('animationSpan');
-          containerContentSpansArray[i].classList.add('spanPageUp');
-          //remove as classes posteriores       
-          if( i < 4) {
-            sliderPagesArray[i+1].classList.remove('sliderUp');  
-            containerContentSpansArray[i+1].classList.remove('spanPageUp');  
-          }
-
+          arrayTest.forEach((item) => {
+            if( i > 0){
+              item[i-1].classList.add('showElement');
+            }
+            item[i-1].classList.remove('mouseScrolled');
+            item[i].classList.add('mouseScrolled');
+            item[i].classList.remove('showElement');
+            if(i < 4){
+              item[i+1].classList.remove('mouseScrolled');
+            }
+          })
         }
     });
     //evento de Click no nav menu
     sliderNavegationLiArray[i].addEventListener('click', (event) => {
-      //sliderPagesArray forEach: para adicionar o sliderUp em todas as paginas, em seguida removendo da pagina selecionada e adicionando o sliderShow na pagina selecionada
       sliderPagesArray.forEach((item, index) => {
-        item.classList.add("sliderUp");
-        item.classList.remove("sliderShow");
+        item.classList.add("mouseScrolled");
+        item.classList.remove("showElement");
       });
-      sliderPagesArray[i].classList.remove('sliderUp');
-      sliderPagesArray[i].classList.add('sliderShow');
-      // removendo o activeSlide de toda as li e adicionando na que foi clicada.
+      sliderPagesArray[i].classList.remove('mouseScrolled');
+      sliderPagesArray[i].classList.add('showElement');
+      // removendo o showElement de toda as li e adicionando na que foi clicada.
       sliderNavegationLiArray.forEach((item, index) => {
-        item.classList.remove('activeSlide')
+        item.classList.remove('showElement')
       });
-      sliderNavegationLiArray[i].classList.add('activeSlide');
+      sliderNavegationLiArray[i].classList.add('showElement');
 
       //Mesma lógica do sliderPagesArray
       containerContentSpansArray.forEach((item, index) => {
-        item.classList.add('spanPageUp');
-        item.classList.remove('animationSpan');
+        item.classList.add('mouseScrolled');
+        item.classList.remove('showElement');
       })
-      containerContentSpansArray[i].classList.remove('spanPageUp');
-      containerContentSpansArray[i].classList.add('animationSpan');
+      containerContentSpansArray[i].classList.remove('mouseScrolled');
+      containerContentSpansArray[i].classList.add('showElement');
     });
 
     var touchY;
@@ -102,22 +81,22 @@
     }
     else if (directionY >  0 && i <= 4){
       //adiciona e remove as classes anteriores
-      sliderNavegationLiArray[i-1].classList.add('activeSlide')
-      sliderPagesArray[i-1].classList.add('sliderShow');
-      sliderPagesArray[i-1].classList.remove('sliderUp');
-      containerContentSpansArray[i-1].classList.add('animationSpan');
-      containerContentSpansArray[i-1].classList.remove('spanPageUp');
+      sliderNavegationLiArray[i-1].classList.add('showElement')
+      sliderPagesArray[i-1].classList.add('showElement');
+      sliderPagesArray[i-1].classList.remove('mouseScrolled');
+      containerContentSpansArray[i-1].classList.add('showElement');
+      containerContentSpansArray[i-1].classList.remove('mouseScrolled');
 
       //adiciona e remove as classes atuais
-      sliderPagesArray[i].classList.remove('sliderShow');
-      sliderPagesArray[i].classList.add('sliderUp');
-      sliderNavegationLiArray[i].classList.remove('activeSlide');
-      containerContentSpansArray[i].classList.remove('animationSpan');
-      containerContentSpansArray[i].classList.add('spanPageUp');
+      sliderPagesArray[i].classList.remove('showElement');
+      sliderPagesArray[i].classList.add('mouseScrolled');
+      sliderNavegationLiArray[i].classList.remove('showElement');
+      containerContentSpansArray[i].classList.remove('showElement');
+      containerContentSpansArray[i].classList.add('mouseScrolled');
 
       //remove as classes posteriores
-      sliderPagesArray[i+1].classList.remove('sliderUp');  
-      containerContentSpansArray[i+1].classList.remove('spanPageUp');      
+      sliderPagesArray[i+1].classList.remove('mouseScrolled');  
+      containerContentSpansArray[i+1].classList.remove('mouseScrolled');      
     }
 }, false);
   }
