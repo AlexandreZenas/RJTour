e7eb61174a495ba1c27defeaf79f1dd51f18cc7d
alexandrezenas <94380403+AlexandreZenas@users.noreply.github.com>
2023-10-19 15:26:35 -0300

    página inicial faltando animação no click

--- src/components/slider-navegation/index.jsx
+++ src/components/slider-navegation/index.jsx
@@ -1,23 +1,16 @@
-import dadosSlides from '../Mocks';
 import '../animations/index.css';
 import './index.css';
-import { forwardRef, useState, useEffect } from 'react';
+import { forwardRef } from 'react';
 
- const SliderNavegation = forwardRef(( props, ref ) =>  {
-  const [ x, setX] = useState();
-  useEffect(() => {
-    setX(props.slideClick)
-  }, [props.slideClick])
-  console.log(x == 'slidePage-2', x)
-  console.log(props.slider)
+ const SliderNavegation = forwardRef((slider, navegationLi) =>  {
   return (
     <nav className='sliderNavegation'>
-        <ol className='numbersOl' ref={ref}>
-            <li onClick={props.handlerClick} id={`slidePage-1`} className={`${props.slider['0']} ${x == 'slidePage-1' ? 'showElement' : ''}`} >1<div className="textLine"></div></li>
-            <li onClick={props.handlerClick} id={`slidePage-2`} className={`${props.slider['1']} ${x == 'slidePage-2' ? 'showElement' : ''}`}>2<div className="textLine"></div></li>
-            <li onClick={props.handlerClick} id={`slidePage-3`} className={`${props.slider['2']} ${x == 'slidePage-3' ? 'showElement' : ''}`}>3<div className="textLine"></div></li>
-            <li onClick={props.handlerClick} id={`slidePage-4`} className={`${props.slider['3']} ${x == 'slidePage-4' ? 'showElement' : ''}`}>4<div className="textLine"></div></li>
-            <li onClick={props.handlerClick} id={`slidePage-5`} className={`${props.slider['4']} ${x == 'slidePage-5' ? 'showElement' : ''}`}>5<div className="textLine"></div></li>
+        <ol className='numbersOl' ref={navegationLi}>
+            <li className={`${slider.slider[0]}`} >1<div className="textLine"></div></li>
+            <li className={`${slider.slider[1]}`}>2<div className="textLine"></div></li>
+            <li className={`${slider.slider[2]}`}>3<div className="textLine"></div></li>
+            <li className={`${slider.slider[3]}`}>4<div className="textLine"></div></li>
+            <li className={`${slider.slider[4]}`}>5<div className="textLine"></div></li>
         </ol>
     </nav>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,45 +4,24 @@ import SliderNavegation from '../../components/slider-navegation';
 import '../../components/animations/index.css'
 import '../../components/slider-navegation/index.css'
 import {useState, useRef, useEffect} from 'react';
-import Slides from '../../components/slides';
 
 
 export default function Home() {
     const slides = useRef({})
     const liRef = useRef(null)
     // const navegationLi = useRef({})
-    const [slider, setSlider] = useState({[0]: 'show'});     
-    const [ slideClick, setSlideClick] = useState();
+    const [slider, setSlider] = useState({[0]: 'showElement'});     
 
-    const handlerClick = (event) => {
-        setSlideClick(event.target.id)
-        // console.log(slideClick, event.target.id)
-        // console.log(slideClick)
-      }  
-    useEffect( () => {      
-   
+    useEffect( () => {            
         const arraySlides = Array.from(slides.current?.childNodes)
         const arrayLiRef = Array.from(liRef.current?.childNodes)
-        //  
-        // slider.forEach((item) => {
-            
-        // });
         for(let i = 0; i < arraySlides.length ; i++){
             //click
-            // arrayLiRef[i].addEventListener('click', () => {                
-                    
-            //         // setSlider({[i]: 'showElement'} )  
-            //         // setSlider(() => {   
-            //         //     return{
-            //         //         ...slider, 
-            //         //         [i]: 'showElement'
-                            
-            //         //     }                 
-            //         // })
-            //         // setSlider('','','','', )
-            //         // console.log(slider)
-            //         //descobrir maneira de selecionar todas as li e passar uma string(classe) pra cada uma 
-            // });
+            arrayLiRef[i].addEventListener('click', () => {                
+                    // setSlider({[i]: 'showElement'} )  
+                    setSlider({[i]: 'showElement'} )  
+                    //descobrir maneira de selecionar todas as li e passar uma string(classe) pra cada uma 
+            });
             
             
             const scrollDown = () => {
@@ -54,7 +33,8 @@ export default function Home() {
             //scroll
             arraySlides[i].addEventListener('wheel', (event) => {
                 if(event.deltaY > 0  && i < 4){
-                    scrollDown();                    
+                    scrollDown();
+                    
                 }
                 else if(event.deltaY < 0  && i > 0){
                     scrollUp();
@@ -62,45 +42,48 @@ export default function Home() {
             })
             
         }
-    },[slideClick ])
+    },[ ])
 
   return (
     <main id='home'>     
         <Header/>
-        <SliderNavegation slider={slider} ref={liRef} slideClick={slideClick} handlerClick={handlerClick}  setSlideClick={setSlideClick} />    
+        <SliderNavegation slider={slider} ref={liRef}/>    
         <section className='mainBanner' ref={slides}>
-            <Slides slideClick={slideClick}/>
                 {/* slidePage-1 */}
-                
-                
+                <div id="slidePage-1" className={`slidePages ${slider[0] }`}  >
+                    <div className='containerContent' > 
+                        <h1 className='mainTitleSlider'>INNOVATIVE <br></br> EXPERIENCE</h1>
+                        <a href='#' target="_parent" rel='noopener noreferrer'><span className={`${slider[0] }`} >ILLUSTRATIVE BUTTON</span></a>
+                    </div>
+                </div>
                 {/* slidePage-2 */}
-                {/* <div id="slidePage-2" className={`slidePages ${slider[1]}`}  >
+                <div id="slidePage-2" className={`slidePages ${slider[1]}`}  >
                     <div className='containerContent'>  
                         <h1>REDEFININDO <br></br> A VIDA DE  LUXO</h1>
                         <a href="#" target="_blank"><span className={`${slider[1] }`}>ILLUSTRATIVE BUTTON</span></a>
                     </div>  
-                </div> */}
+                </div>
                     {/* slidePage-3 */}
-                {/* <div id="slidePage-3" className={`slidePages ${slider[2]}`} >
+                <div id="slidePage-3" className={`slidePages ${slider[2]}`} >
                     <div className='containerContent'>  
                         <h1>LUXO  SELECIONADO</h1>
                         <a href="#" target="_blank"><span className={`${slider[2] }`}>ILLUSTRATIVE BUTTON</span></a>
                     </div>
-                </div> */}
+                </div>
                 {/* slidePage-4 */}
-                {/* <div id="slidePage-4" className={`slidePages ${slider[3]}`} >
+                <div id="slidePage-4" className={`slidePages ${slider[3]}`} >
                     <div className=' containerContent'> 
                         <h1>VIENA 1010</h1>
                         <a href="#" target="_blank"><span className={`${slider[3] }`}>ILLUSTRATIVE BUTTON</span></a>
                     </div>
-                </div> */}
+                </div>
                 {/* slidePage-5 */}
-                {/* <div id="slidePage-5" className={`slidePages  ${slider[4]}`} >
+                <div id="slidePage-5" className={`slidePages  ${slider[4]}`} >
                     <div className= "containerContent"> 
                         <h1>ANDARES DE  LUXO</h1>
                         <a href="#" target="_blank"><span className={`${slider[4] }`}>ILLUSTRATIVE BUTTON</span></a>
                     </div>
-                </div> */}
+                </div>
         </section>
     </main>
   )
